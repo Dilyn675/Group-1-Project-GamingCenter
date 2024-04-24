0039c36e37e2fe76bfe608ea41c21fb9631ff876
Dilyn675 <111885103+Dilyn675@users.noreply.github.com>
2024-04-23 19:21:22 -0500

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,1 +1,10 @@
+CREATE TABLE `categories` (
+  `categoryID` int(11) NOT NULL,
+  `category_title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `brands` (
+  `brandID` int(11) NOT NULL,
+  `brand_title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
