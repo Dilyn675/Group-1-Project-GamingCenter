21dc4fab7d8f5cf62ff2cfefd28fe47a3118bf8c
Dilyn675 <111885103+Dilyn675@users.noreply.github.com>
2024-04-30 17:43:40 -0500

    Update insert_categories.php

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -4,19 +4,21 @@ if(isset($_POST['insert_cat'])){
     $category_title=$_POST['cat_title'];
 
     // select data from database 
-    $select_query="Select * from 'categories' where category_title='$category_title'";
+    $select_query=" Select * from `categories` where category_title='$category_title'";
     $result_select=mysqli_query($con,$select_query);
     $number=mysqli_num_rows($result_select);
     if($number>0){
         echo "<script>alert('This category is prestent inside the database')</script>";
-    }else{
-
-    $insert_query="insert into 'categories' (category_title) values ('$category_title')";
-    $result=mysqli_query($con,$insert_query);
-    if($result){
-        echo "<script>alert('Category has been inserted successfully')</script>";
     }
-}}
+    else{
+
+        $insert_query="insert into `categories` (category_title) values ('$category_title')";
+        $result=mysqli_query($con,$insert_query);
+        if($result){
+            echo "<script>alert('Category has been inserted successfully')</script>";
+        }
+    }
+}
 ?>
 
 <h2 class="text-center">Insert Categories</h2>
